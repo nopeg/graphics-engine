d59c1b1b0503033abbdfdfa29cf2b53f9853b2e1
nopeg <vadimus.05ev@mail.ru>
2024-10-25 15:01:07 +0300

    Drawing wireframe rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,17 @@ int main()
 
 	//tringle vertices
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
 	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
 
 	//vertex buffer object
 	unsigned int VBO;
@@ -42,6 +49,12 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 	//shaders sources
 	std::string vertexShaderString = R"(
@@ -123,6 +136,8 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 	while (!glfwWindowShouldClose(window)) 
 	{
 		glfwPollEvents();
@@ -131,7 +146,8 @@ int main()
 
 		glUseProgram(shaderProgram);
   		glBindVertexArray(VAO);
-  		glDrawArrays(GL_TRIANGLES, 0, 3);
+  		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
